0c89f168d31bdcbb9c7d2617ea891bd4d5719f00
sanderp1 <sander.paulus@cegeka.com>
2017-11-28 14:51:12 +0100

    meer tests

--- src/main/java/Greeter.java
+++ src/main/java/Greeter.java
@@ -1,5 +1,7 @@
 public class Greeter {
     public String greet(String name) {
+        if(name == null)
+            throw new IllegalArgumentException("Screw you");
         return "Hello " + name + "!";
     }
 }
--- src/test/java/GreeterTest.java
+++ src/test/java/GreeterTest.java
@@ -1,14 +1,22 @@
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class GreeterTest {
 
     @Test
-    public void greet_returnHelloName(){
+    public void greet_returnHelloName() {
         Greeter greeter = new Greeter();
         String actual = greeter.greet("Sander");
         assertThat(actual).isEqualTo("Hello Sander!");
+    }
+
+    @Test
+    public void greet_givenNull_thenExplode() {
+        Greeter greeter = new Greeter();
+        assertThatThrownBy(() -> greeter.greet(null)).hasMessageMatching("Screw you");
+
     }
 
 }
